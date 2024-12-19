71da1d9d0bb797969ae5a0376907c4b0adba6445
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2024-12-18 23:44:11 -0600

    Add files via upload

---
+++ Differenceof.java
@@ -0,0 +1,11 @@
+public class Differenceof {
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        //shorten if statement to if boolean is not a weekday or if boolean equals vacation return true
+        if (!weekday || vacation) {
+          return true;
+        }
+        //else return false
+        return false;
+        
+      }
+}
